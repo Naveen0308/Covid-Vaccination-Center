163aa90f5074e6722dce875ac8226bef65969192
Naveen <rnaveen.it2021@citchennai.net>
2024-02-11 09:53:18 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // Set headers to allow cross-origin requests
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://vcb-vaccine-bookings.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 // Endpoint to handle user signup
 app.post('/api/signup', async (req, res) => {
